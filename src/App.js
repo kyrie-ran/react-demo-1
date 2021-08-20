7f936cf4ed97c2c040525e46e27448f7c62e7a60
陈浩然 <1638238157@qq.com>
2021-08-20 16:20:47 +0800

    hook1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import Clock from "./components/Clock";
-import Event from "./components/Event";
-import List from "./components/List";
-import MyForm from "./components/MyForm";
-import MySlot from "./components/MySlot";
-import Router1 from "./components/Router1";
-import Game from "./components/Game";
+// import Clock from "./components/Clock";
+// import Event from "./components/Event";
+// import List from "./components/List";
+// import MyForm from "./components/MyForm";
+// import MySlot from "./components/MySlot";
+// import Router1 from "./components/Router1";
+// import Game from "./components/Game";
+
+import Example from "./hooks/Example";
 
 function App() {
   return (
     <div className="App">
-      <Game />
+      {/* <Game /> */}
       {/* <Clock date={new Date()}/> */}
-      <Clock />
-      <Event />
-      <List />
-      <MyForm />
-      <MySlot left={' left'} right={' right'}>
+      {/* <Clock /> */}
+      {/* <Event /> */}
+      {/* <List /> */}
+      {/* <MyForm /> */}
+      {/* <MySlot left={' left'} right={' right'}>
           children
-      </MySlot>
-      <Router1 />
+      </MySlot> */}
+      {/* <Router1 /> */}
+      <Example />
     </div>
   );
 }
