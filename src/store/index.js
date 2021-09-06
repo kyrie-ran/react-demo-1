a921b359c4f1387e16d206ec8e26e519b7339e97
陈浩然 <1638238157@qq.com>
2021-09-06 22:55:07 +0800

    添加调试

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
-import {createStore,applyMiddleware} from 'redux';
+import {createStore,applyMiddleware,compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
 import reducer from './reducer.js';
 
+// composeEnhancers 函数
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true}) || compose;
+
 // 应用中间件
 const storeEnhancer = applyMiddleware(thunkMiddleware);
 
-const store = createStore(reducer,storeEnhancer);
+const store = createStore(reducer,composeEnhancers(storeEnhancer));
 
 export default store;
