69283b6f945da2d67c64db58d1d22eab706a7262
陈浩然 <1638238157@qq.com>
2021-09-17 15:49:25 +0800

    useRef获取函数组件

--- src/hooks/UseRef.js
+++ src/hooks/UseRef.js
@@ -1,11 +1,16 @@
-import React,{useRef} from 'react';
+import React,{useRef,forwardRef} from 'react';
+
+const FnCom = forwardRef((props,ref) => <input type="text" ref={ref}/>)
 
 export default function UseRef() {
     const titleRef = useRef();
+    const fnRef = useRef();
     console.log(titleRef);
+    console.log(fnRef);
     return (
         <div ref={titleRef}>
             useref
+            <FnCom ref={fnRef}/>
         </div>
     )
 }
